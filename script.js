27f0502b4438b0a49aa680f4dd28a6903314f255
G4BOs <108361351+G4BOs@users.noreply.github.com>
2023-12-07 16:56:01 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 /*Variables*/ 
-let peso = 0;
-let VolumenDiario = 0;
-let Mantenimiento = 0;
-let Mxm2 = 0;
-let SuperficieCorporal = 0;
+
+
+/*Constantes*/
+
+const CALCULAR = document.getElementById('calcular');
+const ERROR = document.getElementById ('error');
+const FLU = document.getElementById ('flu');
+const MAN = document.getElementById ('man')
 
 /*Calculo*/
-
-if (peso <= 10) {VolumenDiario=peso * 100; console.log("Volumen Diario es", VolumenDiario, "cc");
+function calcFlujo(peso)
+{if (peso <= 10) {VolumenDiario=peso * 100; console.log("Volumen Diario es", VolumenDiario, "cc");
 Mantenimiento= VolumenDiario/24;
 console.log("Mantenimiento:",Mantenimiento,"cc/h");
 Mxm2=Mantenimiento*1.5;console.log("MxM/2:",Mxm2,"cc/h");}
@@ -26,6 +29,16 @@ Mxm2=Mantenimiento*1.5;console.log("MxM/2:",Mxm2,"cc/h");
 
 
 else if (peso > 30){SuperficieCorporal = ((peso*4)+7)/(peso+90); console.log("Superficie Corporal es",SuperficieCorporal); console.log("SC x 1500:",SuperficieCorporal*1500, "SC x 2000:",SuperficieCorporal*2000)}
+}
 
-
+/*Event*/
+CALCULAR.addEventListener('click', () => {
+    const DATO = document.getElementById('peso').value;
+    if (DATO > 0){
+        ERROR.style.display = 'none';
+        let VolumenDiario = calcFlujo(DATO);
+    } else {
+        ERROR.style.display = 'block';
+    }
+})
 
